81e00b7348d0dcf38d60127a474cecff78169172
tabbor <143368290+tabbor@users.noreply.github.com>
2023-09-05 09:26:44 +0800

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,30 +1,29 @@
-#include<cstdio>
 #include<iostream>
+
 #define MaxSize 50
+
 using namespace std;
 
 typedef struct linknode{
 	int data;
 	struct linknode* next;
-} LinkNode; 
+} LinkNode;
 
 typedef struct linkqueue{
-	LinkNode* front ,* rear; 
+	LinkNode* front ,* rear;
 }* LinkQueue;
 
-void InitQueue(LinkQueue &Q){
+void InitQueue(LinkQueue &Q) {
 	LinkNode *p = new LinkNode;
 	p->data = 1;
 	p->next = NULL;
 	cout << p;
 	cout << endl;
 	Q->front = p;
-} 
+}
 
-// 带节点链队列 
 int main() {
 	LinkQueue lq;
 	InitQueue(lq);
 	cout << lq->front->data << endl;
-	
-} 
+}
